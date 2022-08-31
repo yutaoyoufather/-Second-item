7971884548fd94277ffdfc326748e80c8ff4b1bb
yutao <2280798335@qq.com>
2022-08-31 16:18:46 +0800

    提交

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# -Second-item
+防抖和节流函数封装
+
+返回一个对象main 引入需要结构化赋值  对象里面有2个函数
+
+{debounce,throttle} 
+
+debounce 防抖函数
+接收两个参数:
+    func:函数
+    delay:时间 多久触发
+返回一个函数
+
+throttle 节流函数
+接收两个参数:
+    time:时间 多久触发,
+    fun:函数
+返回一个函数
+
+
 Anti shake and throttle function encapsulation
+
+
+
+Return an object (main) to introduce, and structural assignment is required
+
+
+{debounce,throttle}
+
+
+
+Debounce anti shake function
+
+Receive two parameters:
+
+Func: function
+
+Delay: how long will it be triggered
+
+Returns a function
+
+
+
+Throttle function
+
+Receive two parameters:
+
+Time: how long is the trigger time,
+
+Fun: function
+
+Returns a function
---
+++ debounce.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * 防抖
+ * @param {Function} func 
+ * @param {Number} delay 
+ * @returns function
+ */
+  function debounce(func,delay) {
+  var timer = null;
+	// 这里返回的函数是每次用户实际调用的防抖函数
+	return function () {
+		// 如果已经设定过计时器了就清除掉上一次的定时器
+		if(timer) { clearTimeout(timer); }
+		var that = this;
+	    var args = arguments;
+	    // 开始一个新的定时器, 延迟执行用户传入的方法
+	    timer = setTimeout(function (){
+	      func.apply(that, args);
+	    },delay);
+	}
+}
+/**
+ * @param {Number} time 
+ * @param {Function} fun 
+ * @returns function();
+ */
+function throttle(time, fun) {
+  var flag = 0;
+  return function () {
+      var now = +new Date().valueOf();
+      if (now - flag >= time) {
+          fun.apply(this, arguments);
+          flag = now;
+      }
+  }
+}
+var main = {
+  debounce: debounce,
+  throttle: throttle
+};
+
+module.exports = main;
